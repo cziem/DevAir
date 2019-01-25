3ff09130eaea16e056a88212dc04facf45096acc
Fayvor George <phavorsparks@gmail.com>
2019-01-25 17:24:04 +0100

    setup the app.js file

---
+++ app.js
@@ -0,0 +1,33 @@
+const express         = require('express'),
+     mongoose        = require('mongoose'),
+     cors            = require('cors'),
+     bodyParser      = require('body-parser')
+
+/* DECLARE APP BINDINGS HERE */
+const app    =  express(),
+     port   =   process.env.PORT,
+     uri    =   process.env.MONGODB_URI,
+     secret =   process.env.APP_SECRET
+
+/* REQUIRE CUSTOM MODULES HERE */
+
+
+/* START DB CONNECTION */
+mongoose.connect(uri, {
+  useCreateIndex: true,
+  useNewUrlParser: true
+}).then(() => console.log(`Connected to DB @: ${uir}`))
+  .catch(err => console.log(`Could not connect to DB. Please refer to Error: ${err.message}`))
+
+/* ALL APP.USE() CALLS HERER */
+
+
+/* APPLICATION ROUTES HERE */
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to DevAir',
+    type: 'You made a GET request to our API'
+  })
+})
+
+app.listen(port, () => console.log(`DevAir running on port ${port}`))
